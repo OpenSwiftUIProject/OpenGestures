d36a9f5fba3c3555675f5d6be074c878c3d947f0
Kyle <kyle201817146@gmail.com>
2026-04-12 20:57:58 +0800

    Add NestedDescription

---
+++ Sources/OpenGestures/Util/NestedDescription.swift
@@ -0,0 +1,120 @@
+//
+//  NestedDescription.swift
+//  OpenGestures
+//
+//  Audited for 9126.1.5
+//  Status: Complete
+
+package struct NestedDescription {
+    package struct Options: OptionSet {
+        package let rawValue: Int
+
+        package init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+
+        package static let hideTypeName = Self(rawValue: 1 << 0)
+        package static let hideIdentity = Self(rawValue: 1 << 1)
+        package static let hideClassAddress = Self(rawValue: 1 << 2)
+        package static let compact = Self(rawValue: 1 << 3)
+    }
+    package var options: Options = []
+    package var customPrefix: String?
+    package var customSuffix: String?
+    package let depth: Int
+    package let target: Any
+    package var buffer: [String] = []
+
+    mutating package func append(
+        _ content: String?,
+        label: String? = nil
+    ) {
+        guard let content else {
+            return
+        }
+        var result: String = ""
+        if let label {
+            result += "\(label): "
+        }
+        result += "\(content)"
+        if !result.isEmpty {
+            buffer.append(result)
+        }
+    }
+
+    // MARK: - Build
+
+    package func buildOpening() -> String {
+        if let customPrefix {
+            return customPrefix
+        }
+        var result: String
+        if !options.contains(.hideTypeName) {
+            result = "\(type(of: target))"
+        } else {
+            result = ""
+        }
+        var hasClassIdentity = false
+        if !options.contains(.hideClassAddress) {
+            let dynamicType = type(of: target)
+            if dynamicType is AnyObject.Type {
+                let obj = target as AnyObject
+                let address = UInt(bitPattern: ObjectIdentifier(obj))
+                let addressString = "0x\(String(address, radix: 16, uppercase: false))"
+                result += " <\(addressString)"
+                hasClassIdentity = true
+            }
+        }
+        if !options.contains(.hideIdentity),
+           let identifiable = target as? any Identifiable {
+            if hasClassIdentity {
+                result += " \(identifiable.id)>"
+            } else {
+                result += " <\(identifiable.id)>"
+            }
+        } else if hasClassIdentity {
+            result += ">"
+        }
+        result += result.isEmpty ? "" : " "
+        result += "{"
+        result += buffer.isEmpty ? "" : " "
+        return result
+    }
+
+    package func buildBody() -> String {
+        guard !options.contains(.compact) else {
+            return buffer.joined(separator: ", ")
+        }
+        let separator: String
+        if buffer.isEmpty {
+            separator = ""
+        } else {
+            let depth = depth + 1
+            let indent = String(repeating: " ", count: 2)
+            let indentWithDepth = String(repeating: indent, count: depth)
+            separator = "\n" + indentWithDepth
+        }
+        return separator + buffer.joined(separator: separator)
+    }
+
+    package func buildClosing() -> String {
+        var result = customSuffix ?? "}"
+        guard !buffer.isEmpty else {
+            return result
+        }
+        let leading: String
+        if options.contains(.compact) {
+            leading = customSuffix == nil ? " " : ""
+        } else {
+            let indent = String(repeating: " ", count: 2)
+            let indentWithDepth = String(repeating: indent, count: depth)
+            leading = "\n\(indentWithDepth)"
+        }
+        result = leading + result
+        return result
+    }
+
+    package var description: String {
+        buildOpening() + buildBody() + buildClosing()
+    }
+}
